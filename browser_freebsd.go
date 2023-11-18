ab4e5771649df7a2a080926318307ce3e35f3218
YOU 98234789897 289 73ha 89 929 834789 78923789 4789 234 <60656569+sieioloadclie2323@users.noreply.github.com>
2023-11-18 00:25:14 -0600

    Update browser_freebsd.go

--- browser_freebsd.go
+++ browser_freebsd.go
@@ -1,15 +1,28 @@
-package browser
+234
 
-import (
-	"errors"
-	"fmt"
-	"os/exec"
-)
 
-func openBrowser(url string) error {
-	err := runCmd("xdg-open", url)
-	if errors.Is(err, exec.ErrNotFound) {
-		return fmt.Errorf("%w - install xdg-utils from ports(8)", err)
-	}
-	return err
-}
+ 23 4
+j  2
+o342 op i no23 ioppoi  234 
+
+
+h
+a
+ha
+ha
+ha
+ha
+ha
+ha
+ha
+ha
+ha
+ha
+ah
+h  sklort 82389 4 98y2 8934 980y2 980y34 9y80 98 
+
+2
+ 34 
+
+ 23
+ 234 inop 2ipo34 pino niop23 niop4 nopi2 3niop4pio n niop2 io33
