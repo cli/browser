9c8f9053d23e40d87ff08909a5235bf82fa96a9f
Jan-Otto Kröpke <mail@jkroepke.de>
2026-06-21 12:32:37 +0200

    feat: Add context-aware functions
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- browser_windows.go
+++ browser_windows.go
@@ -1,7 +1,11 @@
 package browser
 
-import "golang.org/x/sys/windows"
+import (
+	"context"
 
-func openBrowser(url string) error {
+	"golang.org/x/sys/windows"
+)
+
+func openBrowser(_ context.Context, url string) error {
 	return windows.ShellExecute(0, nil, windows.StringToUTF16Ptr(url), nil, nil, windows.SW_SHOWNORMAL)
 }
